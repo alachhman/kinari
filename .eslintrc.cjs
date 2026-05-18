131c280946ff64ebdd026230ca587602272491f4
Anthony Lachhman <33943685+alachhman@users.noreply.github.com>
2026-05-18 17:16:13 -0400

    Set up ESLint and Prettier at workspace root

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  settings: { react: { version: "19" } },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
+  },
+  ignorePatterns: ["dist/", "node_modules/", "*.config.{js,ts,mjs,cjs}"],
+};
