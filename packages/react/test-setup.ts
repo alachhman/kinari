c0a703cefba1d99ad5730a4cffc2fdedc4cbcaa2
Anthony Lachhman <33943685+alachhman@users.noreply.github.com>
2026-05-18 17:25:20 -0400

    Create @kinari/react package skeleton (React 19, Vitest, jsdom)

---
+++ packages/react/test-setup.ts
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom/vitest";
+import { afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+
+afterEach(() => {
+  cleanup();
+});
+
+// Mock matchMedia for usePrefersReducedMotion tests
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    addListener: () => {},
+    removeListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
